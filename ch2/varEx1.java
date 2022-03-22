c496fcd0356cdf1fc246b081c6d1b6dcb47ecb25
yimdonggyu <dgyim@yimdonggyus-MacBook-Pro.local>
2022-03-22 19:10:18 +0900

    chapter2 update

---
+++ ch2/varEx1.java
@@ -0,0 +1,12 @@
+class varEx1 {
+    public static void main(String[] args){
+        int year = 0;
+        int age = 14;
+
+        System.out.println(year);
+        System.out.println(age);
+
+        //int noInit;
+        //System.out.println(noInit); //No initialize Error 
+    }
+}
